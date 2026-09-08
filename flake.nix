f848b8417c371f55ca7442fbee0f58f94092c250
zignis <77036902+zignis@users.noreply.github.com>
2026-09-08 11:48:52 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "libbpf starter template";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.mkShell {
+          name = "libbpf-template";
+          nativeBuildInputs = with pkgs; [
+            clang
+            llvm
+            pkg-config
+            openssl
+          ];
+
+          packages = with pkgs; [
+            elfutils
+            zlib
+          ];
+
+          # See prior art in nixpkgs at:
+          # * https://github.com/NixOS/nixpkgs/issues/325910
+          # * https://github.com/NixOS/nixpkgs/pull/325587
+          hardeningDisable = [
+            "zerocallusedregs"
+          ];
+        };
+      }
+    );
+}
+
